c9d4dd9ea666e65ff85c8f5d22d1a48c666a393d
Manar Hielal <mannar.hielal@liip.ch>
2019-09-28 10:15:35 +0200

    add dogs data and grab them in pets.vue

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -7,6 +7,14 @@
           <v-card-title>
             <div>
               <h3>Looking for a dog?</h3>
+              <!-- <ul>
+                <li v-for="key in dogs">{{key.name}}- {{key.breed}}</li>
+              </ul>-->
+              <v-flex xs12 sm4 md3 v-for="pet in dogs" v-bind:key="pet.breed">
+                <h3>{{pet.name}}</h3>
+                <p>{{pet.breed}}</p>
+                <v-img v-bind:src="pet.img" height="170px"></v-img>
+              </v-flex>
             </div>
           </v-card-title>
         </v-card>
@@ -14,6 +22,16 @@
     </v-layout>
   </v-container>
 </template>
+<script>
+import { Dogs } from "../data/dogs";
+export default {
+  data() {
+    return {
+      dogs: Dogs
+    };
+  }
+};
+</script>
 
 <style scoped>
 p {
