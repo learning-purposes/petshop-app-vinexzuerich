463473912d701105835eef2aab2ba6cb23e27ee6
Manar Hielal <mannar.hielal@liip.ch>
2019-09-28 14:54:55 +0200

    Randomize images in pets

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -3,11 +3,13 @@
     <v-layout wrap>
       <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
         <DogItem v-bind="pet"></DogItem>
-      </v-flex>
+      </v-flex>>
     </v-layout>
   </v-container>
 </template>
 <script>
+import axios from "axios";
+axios.defaults.baseURL = "https://dog.ceo/api";
 import DogItem from "@/components/DogItem";
 
 import { Dogs } from "../data/dogs";
@@ -19,6 +21,27 @@ export default {
     return {
       dogs: Dogs
     };
+  },
+  methods: {},
+  async created() {
+    const linksArray = this.dogs.map(
+      dog => `/breed/${dog.breed}/images/random`
+    );
+
+    // let dog=function(){
+    //   return `/breed/${dog.breed}/images/random`
+    // }
+
+    let response;
+    try {
+      response = await axios.all(linksArray.map(link => axios.get(link)));
+    } catch (e) {
+      throw Error(e);
+    }
+
+    this.dogs.forEach((dog, index) => {
+      dog.img = response[index].data.message;
+    });
   }
 };
 </script>
